508da3f58d24e63b1e3672dce824767dd4340627
tomykaira <tomykaira@gmail.com>
2019-02-24 14:32:11 +0900

    Impl mouse

--- keyboards/helix/cave/keymaps/default/keymap.c
+++ keyboards/helix/cave/keymaps/default/keymap.c
@@ -5,7 +5,7 @@ extern keymap_config_t keymap_config;
 #define BASE 0
 #define META 1
 #define SYMB 2
-#define SKY 3
+#define MOUSE 3
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -31,16 +31,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( \
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_RPRN,  KC_AMPR, KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS , \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DLR,   KC_BSPC, KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_LBRC , \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,   KC_B   , KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RBRC , \
-    KC_GRV,   KC_CIRC, KC_LALT, KC_LGUI, MO(SYMB),KC_SPC,  MO(META), KC_EQL , KC_LSFT, KC_UNDS, KC_BSPC, KC_EQL, KC_AT,   KC_BSLS,   \
-    _______, _______,  _______, KC_ESC , KC_ENT  ,KC_SPC,  _______,  _______, KC_ENT,  KC_UNDS, KC_BSPC, KC_EQL, _______, _______    \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,   MO(MOUSE), KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RBRC , \
+    KC_GRV,   KC_CIRC, KC_LALT, KC_LGUI, MO(SYMB),KC_SPC,  MO(META), KC_EQL , KC_LSFT, KC_UNDS, MO(MOUSE), KC_EQL, KC_AT,   KC_BSLS,   \
+    _______, _______,  MO(MOUSE), KC_ESC , KC_ENT  ,KC_SPC,  _______,  _______, KC_ENT,  KC_UNDS, KC_BSPC, KC_EQL, _______, _______    \
   ),
 
   [META] = LAYOUT( \
     KC_ESC,  KC_EXLM, KC_DQUO, KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_AMPR, KC_QUOT, KC_LPRN, KC_RPRN, KC_MINS, KC_MINS,  \
     _______, _______, _______, KC_MS_U, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
     _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, KC_MINS, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,  \
-    _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_MINS, _______, KC_BTN1, KC_BTN2, _______, _______, TG(SKY) ,  \
+    _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_MINS, _______, KC_BTN1, KC_BTN2, _______, _______, _______,  \
     _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_MINS, _______, KC_BTN1, KC_BTN2, _______, _______, _______   \
   ),
 
@@ -52,11 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______,   KC_UNDS, _______, _______, KC_DOT,  KC_0,    KC_EQL,  _______ \
   ),
 
-
-  [SKY] = LAYOUT( \
-    _______, KCK_COMMA,KC_W,   KC_R   , KC_M   , KC_H   , _______,   _______, KCK_UU,  KCK_AI,  KCK_OU,  KCK_DOT, KCK_EI,   _______,\
-    _______, KC_N,     KC_T,   KC_S   , KC_K   , KC_Y   , _______,   _______, KC_U,    KC_A,    KC_O,    KC_I,    KC_E,     KCK_LBRC,\
-    _______, KC_P,     KC_D,   KC_Z   , KC_G   , KC_B   , _______,   _______, KCK_UN,  KCK_AN,  KCK_ON,  KCK_IN,  KCK_EN,   _______,\
+  [MOUSE] = LAYOUT( \
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_RPRN,  KC_AMPR, KC_Y,   KC_WH_L, KC_WH_D, KC_WH_R,KC_P,    KC_MINS , \
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DLR,   KC_BSPC, KC_H,   KC_BTN1, KC_WH_U, KC_BTN2,KC_SCLN, KC_LBRC , \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,   KC_B   , KC_N,   KC_M,    KC_BTN3, KC_DOT, KC_SLSH, KC_RBRC , \
     _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______,  _______,\
     _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______,  _______ \
   )
